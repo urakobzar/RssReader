e587801e671b420e709ff897b3ce7cc3e59c04b4
Юрий Кобзарь <urakobzar2001@yandex.ru>
2023-04-05 23:07:49 +0700

    Добавьте файлы проекта.

---
+++ RssReader/Default.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="RssReader._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <h2>Read RSS feed from Habr.com.</h2>
+    <div style="max-height: 600px; overflow: auto">
+
+        <asp:GridView ID="gvRss" runat="server" AutoGenerateColumns="false" ShowHeader="false">
+            <Columns>
+                <asp:TemplateField>
+                    <ItemTemplate>
+                        <table border="0" cellpadding="0" cellspacing="5">
+                            <tr>
+                                <td>
+                                    <h3 style="color:indigo"><%# Eval("Title") %></h3>
+                                </td>
+                                <td width="200px">
+                                    <%#Eval("PublishDate") %>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td colspan="2">
+                                    <hr/>
+                                    <%#Eval("Description") %>
+                                </td>
+                            </tr>
+                        </table>
+                    </ItemTemplate>
+                
+                </asp:TemplateField>
+            </Columns>
+        </asp:GridView>
+
+    </div>
+
+</asp:Content>
---
+++ RssReader/Default.aspx.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml.Linq;
+
+namespace RssReader
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                PopulateRssFeed();
+            }
+        }
+
+        private void PopulateRssFeed()
+        {
+            string RssFeedUrl = "https://habr.com/ru/rss/all/all/";
+            List<Feeds> feeds = new List<Feeds>();
+            try
+            {
+                XDocument xDoc = new XDocument();
+                xDoc = XDocument.Load(RssFeedUrl);
+                var items = (from x in xDoc.Descendants("item")
+                    select new
+                    {
+                        title = x.Element("title").Value,
+                        link = x.Element("link").Value,
+                        pubDate = x.Element("pubDate").Value,
+                        description = x.Element("description").Value
+                    });
+                if (items != null)
+                {
+                    foreach (var i in items)
+                    {
+                        Feeds f = new Feeds
+                        {
+                            Title = i.title,
+                            Link = i.link,
+                            PublishDate = i.pubDate,
+                            Description = i.description
+                        };
+
+                        feeds.Add(f);
+                    }
+                }
+
+                gvRss.DataSource = feeds;
+                gvRss.DataBind();
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
